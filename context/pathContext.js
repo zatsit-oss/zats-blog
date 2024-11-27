3a215069ba49a2b58472f0c2a39ced1bda167308
Sylvain YNER <46351019+sylvscrp@users.noreply.github.com>
2024-11-27 17:23:15 +0100

    feat(context): update context

--- context/pathContext.js
+++ context/pathContext.js
@@ -1,22 +1,18 @@
-// src/context/PathUrlContext.js
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext } from "react";
 
 const PATH_URL = {
   website: "https://blog.zatsit.fr",
   blog: "/blog",
   socialShare: {
     x: "https://x.com/intent/post?url=",
-    linkedin: "https://www.linkedin.com/shareArticle/?mini=true&url="
-  }
+    linkedin: "https://www.linkedin.com/shareArticle/?mini=true&url=",
+  },
 };
 
-// Create the context
 const PathUrlContext = createContext(PATH_URL);
 
-// Custom hook to use the context
 export const usePathUrlContext = () => useContext(PathUrlContext);
 
-// Provider component (optional if you plan to add dynamic state in the future)
 export const PathUrlProvider = ({ children }) => {
   return (
     <PathUrlContext.Provider value={PATH_URL}>
