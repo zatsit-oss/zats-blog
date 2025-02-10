7ecb7a47b1fe2d748ee73c560644d75fac8aac28
Célia Doolaeghe <cdoolaeghe@zatsit.fr>
2025-02-10 15:53:37 +0100

    fix(accessibility): improve alt on home page links

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -7,6 +7,7 @@ const FeatureList = [
     title: 'Écrire pour partager',
     Svg: require('@site/static/img/learning-amico.svg').default,
     urlSvg: '/blog',
+    ariaLabel: 'Blog',
     description: (
       <>
         Nos consultants ont du talent et souhaitent vous partager leurs connaissances, leurs valeurs. N'hésite pas à partager si tu aimes.
@@ -17,6 +18,7 @@ const FeatureList = [
     title: 'Simple, efficace et surtout éco-conçu.',
     Svg: require('@site/static/img/website-designer-cuate.svg').default,
     urlSvg: '/blog-conception',
+    ariaLabel: 'Éco-conception du blog',
     description: (
       <>
         Ce blog a été entièrement généré et ne nécessite pas de ressources gourmandes 24/7.
@@ -27,6 +29,7 @@ const FeatureList = [
     title: 'Si tu aimes ce que tu lis',
     Svg: require('@site/static/img/resume-amico.svg').default,
     urlSvg: 'mailto:emplois@zatsit.fr',
+    ariaLabel: 'Envoyer un mail à emplois@zatsit.fr',
     description: (
       <>
         Tu pourrais aussi faire partie de l'aventure ! Rejoindre une équipe motivée par la qualité, la sobriété et le partage.
@@ -36,11 +39,13 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, urlSvg,title, description}) {
+function Feature({Svg, urlSvg, title, ariaLabel, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <a href={urlSvg} aria-label="Visit storyset website for amazing illustrations"><Svg className={styles.featureSvg} role="img" /></a>
+        <a href={urlSvg}>
+          <Svg className={styles.featureSvg} role="img" aria-label={ariaLabel}/>
+        </a>
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h2">{title}</Heading>
@@ -59,7 +64,7 @@ export default function HomepageFeatures() {
             <Feature key={idx} {...props} />
           ))}
         </div>
-          <p className="storySetAttribution">The illustrations of this page comes from <b><a href={"https://storyset.com/"} aria-label="Site de ressources graphiques Storyset">Storyset</a></b></p>
+          <p className="storySetAttribution" lang="en">The illustrations of this page comes from <b><a href={"https://storyset.com/"}>Storyset</a></b></p>
       </div>
     </section>
   );
